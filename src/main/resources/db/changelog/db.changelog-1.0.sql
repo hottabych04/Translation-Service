ed3b629ee4354079939eaa3e695d677d95388df3
hottabych04 <hottabych04@gmail.com>
2024-08-10 00:35:22 +0300

    Add db migration

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset hottabych04:1
+CREATE TABLE IF NOT EXISTS translate
+(
+    id BIGSERIAL PRIMARY KEY ,
+    ip VARCHAR(16) NOT NULL ,
+    source_text TEXT NOT NULL ,
+    translate_text TEXT NOT NULL
+);
